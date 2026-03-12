e5b902e80ed1d735a7f1dc6b0f7c0d313492a841
Fneifnox <tim.stier9@gmail.com>
2026-03-12 19:48:21 +0100

    1.5.3 Update

--- src/main/java/net/fneifnox/extraentityattributes/attributes/foodSpeed/EatingSpeedMixin.java
+++ src/main/java/net/fneifnox/extraentityattributes/attributes/foodSpeed/EatingSpeedMixin.java
@@ -7,6 +7,7 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.util.Hand;
 import net.minecraft.util.UseAction;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 
 // Code was copied from the mod Artifacts. Check out Artifacts!
@@ -15,16 +16,20 @@ public class EatingSpeedMixin {
 
     @ModifyExpressionValue(method = "setCurrentHand", at = @At(value = "INVOKE", target = "Lnet/minecraft/item/ItemStack;getMaxUseTime(Lnet/minecraft/entity/LivingEntity;)I"))
     private int changeEatingSpeed(int original, Hand hand) {
-        LivingEntity entity = (LivingEntity) (Object) this;
-        return changeEatDuration(original, entity.getStackInHand(hand), entity);
+        LivingEntity livingEntity = (LivingEntity) (Object) this;
+        if (livingEntity.getAttributeInstance(ExtraEntityAttributes.EATING_SPEED) == null || livingEntity.getAttributeInstance(ExtraEntityAttributes.DRINKING_SPEED) == null) {
+            return original;
+        }
+        return changeEatAndDrinkDuration(original, livingEntity.getStackInHand(hand), livingEntity);
     }
 
-    private static int changeEatDuration(int originalDuration, ItemStack item, LivingEntity entity) {
+    @Unique
+    private static int changeEatAndDrinkDuration(int originalDuration, ItemStack item, LivingEntity livingEntity) {
 
         if (item.getUseAction() == UseAction.EAT) {
-            return (int) Math.max(1, Math.round(originalDuration / entity.getAttributeValue(ExtraEntityAttributes.EATING_SPEED)));
+            return (int) Math.max(1, Math.round(originalDuration / livingEntity.getAttributeValue(ExtraEntityAttributes.EATING_SPEED)));
         } else if (item.getUseAction() == UseAction.DRINK) {
-            return (int) Math.max(1, Math.round(originalDuration / entity.getAttributeValue(ExtraEntityAttributes.DRINKING_SPEED)));
+            return (int) Math.max(1, Math.round(originalDuration / livingEntity.getAttributeValue(ExtraEntityAttributes.DRINKING_SPEED)));
         }
         return originalDuration;
     }
